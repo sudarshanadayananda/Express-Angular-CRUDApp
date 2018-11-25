fe18bcae20c9dab2d3c8cb79ed44131dbd0e3d8e
sudarshanadayananda <sudarshanadayananda@gmail.com>
2018-11-25 23:53:29 +0530

    Create user if email is not exists in DB

--- backend/api/resources/user/user.controller.js
+++ backend/api/resources/user/user.controller.js
@@ -5,9 +5,13 @@ export default {
     async createUser (req, res) {
 
         try {
-            
-            const user = await User.create(req.body);
-            return res.send({ status: 'SUCCESS', data: user });
+            const foundUser = await User.findOne({email: req.body.email});
+            if (!foundUser) {
+                const user = await User.create(req.body);
+                return res.send({ status: 'OK', message: 'CREATED', data: user });
+            } else {
+                return res.send({ status: 'OK', message: 'EXISTS' });
+            }
         } catch (error) {
             
             return res.status(500).send(error);
