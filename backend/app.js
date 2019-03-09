1e10974823f1e1038c8ecdb3f4a83dcbc174cd6e
sudarshanadayananda <sudarshanadayananda@gmail.com>
2019-03-10 01:01:19 +0530

    Added middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,50 +1,21 @@
 import express from 'express';
-import { connect } from './config/db';
+import DB from './config/db';
+import CORS from './middlewares/cors';
+import notFound from './middlewares/notFound';
+import error from './middlewares/error';
 import { restRouter } from './api';
 
 const app = express();
 const PORT = 3000;
 
-connect();
+// crate database connection
+DB.connect();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
+app.use(CORS.handleCors);
 app.use('/api', restRouter);
-
-app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.message = 'Invalid route';
-    error.status = 404;
-    next(error);
-  });
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  return res.json({
-    error: {
-      message: error.message,
-    },
-  });
-});
+app.use(notFound);
+app.use(error);
 
 app.listen(PORT, () => {
     console.log(`Server is running at PORT http://localhost:${PORT}`);
